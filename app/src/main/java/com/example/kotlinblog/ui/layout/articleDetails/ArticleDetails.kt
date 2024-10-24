a0b83f8bbba93f3bf3686196a0c097bf883a0fe9
Edgar Muyomba <edgarmatthew>
2024-10-24 11:49:13 +0300

    feat: article details page

--- app/src/main/java/com/example/kotlinblog/ui/layout/articleDetails/ArticleDetails.kt
+++ app/src/main/java/com/example/kotlinblog/ui/layout/articleDetails/ArticleDetails.kt
@@ -6,37 +6,51 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.BookmarkBorder
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.kotlinblog.models.Article
 import com.example.kotlinblog.ui.layout.homescreen.article
+import com.example.kotlinblog.utils.Utilities
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArticleDetailsPage(article: Article, modifier: Modifier = Modifier) {
 
+    val scrollState = rememberScrollState()
 
     Scaffold { contentPadding ->
         Box(
@@ -51,8 +65,103 @@ fun ArticleDetailsPage(article: Article, modifier: Modifier = Modifier) {
                 contentDescription = "image of " + article.title,
                 contentScale = ContentScale.Crop
             )
-            Column {
-                CustomTopBar(modifier = Modifier.padding(16.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.6f)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black.copy(alpha = 0.7f)
+                            ),
+                            startY = 0f,  // Gradient starts from top
+                            endY = Float.POSITIVE_INFINITY  // Gradient ends at bottom
+                        )
+                    )
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+            ) {
+                CustomTopBar(modifier = Modifier.padding(top = 16.dp))
+                Spacer(modifier = Modifier.height(300.dp))
+                Text(
+                    text = article.title,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    style = TextStyle(
+                        fontSize = 25.sp,
+                        color = Color.White
+                    )
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {
+                    Text(
+                        text = article.author ?: "Unknown Author",
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            color = Color.White
+                        )
+                    )
+                    Text(
+                        text = "  •  " + Utilities().getRelativeTime(article.publishedAt),
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            color = Color.White
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Box(
+                    modifier = Modifier
+                        .border(
+                            width = 0.dp,
+                            shape = RoundedCornerShape(25),
+                            color = Color.Transparent
+                        )
+                        .background(color = Color.White, shape = RoundedCornerShape(10))
+                        .fillMaxWidth()
+                        .padding(24.dp)
+                        .defaultMinSize(minHeight = 500.dp)
+                ) {
+                    Column(
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = article.source.name ?: "",
+                                style = TextStyle(
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontSize = 24.sp,
+                                )
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Box(
+                                modifier = Modifier
+                                    .background(
+                                        color = Color(0xff2668d1),
+                                        shape = RoundedCornerShape(50)
+                                    )
+                                    .padding(1.5.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Check,
+                                    contentDescription = "Verified",
+                                    modifier = Modifier.height(10.dp),
+                                    tint = Color.White
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = article.content ?: "No content available"
+                        )
+                    }
+                }
             }
         }
 
@@ -67,12 +176,8 @@ fun CustomTopBar(modifier: Modifier = Modifier) {
     ) {
         Box(
             modifier = Modifier
-                .border(
-                    width = 1.dp,
-                    color = Color(0xffededed),
-                    shape = RoundedCornerShape(50)
-                )
-                .background(color = Color(0xffededed), shape = RoundedCornerShape(50))
+                .background(color = Color.White.copy(alpha = 0.2f), shape = RoundedCornerShape(50))
+                .blur(radius = 1.dp)
         ) {
             IconButton(onClick = {}) {
                 Icon(
@@ -86,10 +191,11 @@ fun CustomTopBar(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .border(
                     width = 1.dp,
-                    color = Color(0xffededed),
+                    color = Color.Transparent,
                     shape = RoundedCornerShape(50)
                 )
-                .background(color = Color(0xffededed), shape = RoundedCornerShape(50))
+                .background(color = Color.White.copy(alpha = 0.2f), shape = RoundedCornerShape(50))
+                .blur(radius = 1.dp)
         ) {
             IconButton(modifier = Modifier.padding(0.dp), onClick = {}) {
                 Icon(
@@ -103,10 +209,11 @@ fun CustomTopBar(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .border(
                     width = 1.dp,
-                    color = Color(0xffededed),
+                    color = Color.Transparent,
                     shape = RoundedCornerShape(50)
                 )
-                .background(color = Color(0xffededed), shape = RoundedCornerShape(50))
+                .background(color = Color.White.copy(alpha = 0.2f), shape = RoundedCornerShape(50))
+                .blur(radius = 1.dp)
         ) {
             IconButton(modifier = Modifier.padding(0.dp), onClick = { }) {
                 Icon(
